31f9c79925628714573fb34a0197d949a558d383
Yasas Karunarathna <yasasmail@gmail.com>
2017-11-11 21:54:49 +0530

    Adding rest of the files

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector : 'app-warning-alert',
+    templateUrl : './warning-alert.component.html',
+    styles : [`
+        h2 {
+            color : red
+        }
+    `]
+})
+
+export class WarningAlertComponent {
+
+}
